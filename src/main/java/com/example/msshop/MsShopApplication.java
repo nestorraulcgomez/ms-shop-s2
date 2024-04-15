0a0e5391b5976b81d5c51d7578ecf3d6b27b0bf8
nestorraulcg <nestorraulcgomez@gmail.com>
2024-04-15 17:36:38 -0400

    first commit

---
+++ src/main/java/com/example/msshop/MsShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.msshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msshop/MsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
