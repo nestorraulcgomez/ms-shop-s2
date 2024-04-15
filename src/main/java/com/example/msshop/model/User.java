68327378627d3adc44d7b8a5bd9b6d7136ace2a5
nestorraulcg <nestorraulcgomez@gmail.com>
2024-04-15 17:48:05 -0400

    user model was created

---
+++ src/main/java/com/example/msshop/model/User.java
@@ -0,0 +1,50 @@
+package com.nrcg.ms_shop_s2.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank(message = "No puede ingresar un nombre vacio")
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "lastname")
+    private String lastname;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+}
